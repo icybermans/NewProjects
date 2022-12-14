452f3fd892fecb6834ab2f5b37ff478d5f3cc936
Mansur <cybermans@example.com>
2022-12-14 20:40:32 +0300

    Task from lecture on C#

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,27 @@
+﻿int a = 5;
+int b = 11;
+int c = 5;
+int d = 8;
+int e = 3;
+
+int max = a;
+if(b > max)
+{
+    max = b;
+}
+else{
+    if(c > max){
+        max = c;
+    }
+    else{
+        if(d > max){
+            max = d;
+        }
+        else{
+            if(e > max){
+                max = e;
+            }
+        }
+    }
+}
+Console.Write("Наибольшее число равно: " + max);
