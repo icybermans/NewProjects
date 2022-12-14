9687f502116fdec9de888862c955130ee6253836
Mansur <cybermans@example.com>
2022-12-14 20:19:01 +0300

    Conditional part

---
+++ Example005_ConditionHello/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите свое имя: ");
+
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Урррааа! Это МАША!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username);
+}
